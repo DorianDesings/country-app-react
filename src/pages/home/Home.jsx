36968d65af47c3ac1ab3c5321570dc0ec66aed5d
Dorian <dorianoriginaldesings@gmail.com>
2023-04-13 11:14:56 +0200

    first try

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import Header from '../../components/header/Header';
+import MainContainer from '../../components/main-container/MainContainer';
+
+const Home = () => {
+	return (
+		<>
+			<Header />
+			<MainContainer />
+		</>
+	);
+};
+
+export default Home;
